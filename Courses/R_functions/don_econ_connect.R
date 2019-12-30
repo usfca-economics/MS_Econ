446c60772a7a878cd45b35f68962f891d82c18a8
goughgorski <goughgorski@gmail.com>
2019-12-30 14:09:29 -0800

    added labs and lectures subfolders and modified db connect function

--- Courses/R_functions/don_econ_connect.R
+++ Courses/R_functions/don_econ_connect.R
@@ -2,9 +2,17 @@ don_econ_connect <- function() {
   if (!"RPostgreSQL" %in% installed.packages()) {install.packages("RPostgreSQL")}
   
   library(RPostgreSQL)
+  
+  # Establish driver
   drv <- dbDriver("PostgreSQL")
+  
+  # Close existing connections
+  lapply(dbListConnections(drv), dbDisconnect)
+  
+  # Establish connection
   con <- dbConnect(drv, dbname="don-econ",
                  host="35.230.7.96",port=5432,user="usfstudent",password="dons")
   
+  # Assign connection object to Global Environment
   assign('con', con, globalenv())
 }
